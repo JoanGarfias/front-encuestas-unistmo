3ef08b3e94809fa39648f9ef611a3d4c293d1ff2
ChrisZys <crissmares07@gmail.com>
2025-10-04 10:21:55 -0600

    Add routes (index, formulario, estadisticas)

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,7 @@
-<script setup lang="ts">
-import { Button } from '@/components/ui/button'
-import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from '@/components/ui/accordion'
-
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
-  <Button>Pepesech</Button>
-
-  <Accordion type="single" collapsible>
-    <AccordionItem value="item-1">
-      <AccordionTrigger>Is it accessible?</AccordionTrigger>
-      <AccordionContent>
-        Yes. It adheres to the WAI-ARIA design pattern.
-      </AccordionContent>
-    </AccordionItem>
-  </Accordion>
-
+  <router-view />
 </template>
 
 <style scoped></style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,28 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from "vue-router"
+
+// Aqui importen sus componentes
+// import Example from "../components/example.vue"
+// import Example2 from "../components/example2.vue"
+// import Example3 from "../components/example3.vue"
+
+const routes = [
+  {
+    path: "/",
+    // component: Example,
+  },
+  {
+    path: "/formulario",
+    // component: Example2,
+  },
+  {
+    path: "/estadisticas",
+    // component: Example3,
+  },
+] as any
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes,
 })
 
 export default router
